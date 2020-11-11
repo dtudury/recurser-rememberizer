4e9c9b306d02efdf23fdd0ca279235f9c361ef5e
David Tudury <david.tudury@gmail.com>
2020-11-10 16:43:31 -0800

    more copy paste

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-var hackerschool = require('hackerschool-api'),
-    app = require('express')();
+var hackerschool = require('hackerschool-api')
+var app = require('express')()
+require('dotenv').config()
 
 var client = hackerschool.client();
 
 var auth = hackerschool.auth({
-  client_id: process.env.CLIENT_ID,
-  client_secret: process.env.CLIENT_SECRET,
-  redirect_uri: process.env.REDIRECT_URI
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    redirect_uri: process.env.REDIRECT_URI
 });
 
-app.get('/login', function(req, res) {
-  var authUrl = auth.createAuthUrl();
+app.get('/login', function (req, res) {
+    var authUrl = auth.createAuthUrl();
 
-  // redirect the user to the auth page
-  res.redirect(authUrl);
+    // redirect the user to the auth page
+    res.redirect(authUrl);
 });
 
-app.get('/oauthCallback', function(req, res) {
-  var code = req.query.code;
+app.get('/oauthCallback', function (req, res) {
+    var code = req.query.code;
 
-  auth.getToken(code)
-  .then(function(token) {
-    // tells the client instance to use this token for all requests
-    client.setToken(token);
-  }, function(err) {
-    res.send('There was an error getting the token');
-  });
+    auth.getToken(code)
+        .then(function (token) {
+            // tells the client instance to use this token for all requests
+            client.setToken(token);
+        }, function (err) {
+            res.send('There was an error getting the token');
+        });
 });
 
 app.listen(3000);
