1a5af740641d866df55ac1c04b839319040e7aba
David Tudury <david.tudury@gmail.com>
2020-11-11 18:03:54 -0800

    loading batches

--- static/rememberizer.js
+++ static/rememberizer.js
@@ -1,11 +1,11 @@
-import { proxy, render, h } from './horseless.0.5.1.min.esm.js'
+import { proxy, render, h, mapEntries } from './horseless.0.5.1.min.esm.js'
 
 const model = proxy({
     people: {},
     batches: {}
 })
 
-async function hitRememberizerApi (path) {
+async function hitRememberizerApi(path) {
     try {
         const res = await fetch(path)
         if (!res.ok) {
@@ -19,23 +19,23 @@ async function hitRememberizerApi (path) {
     }
 }
 
-async function getMe () {
+async function getMe() {
     model.me = await hitRememberizerApi('/me')
 }
-async function getPerson (id) {
+async function getPerson(id) {
     const person = await hitRememberizerApi(`/person/${id}`)
     people[id] = person
 }
-async function getList () {
+async function getList() {
     const list = await hitRememberizerApi('/list')
     list.forEach(batch => {
         model.batches[batch.id] = batch
     })
 }
-async function getBatch (id) {
+async function getBatch(id) {
     return hitRememberizerApi(`/batch/${id}`)
 }
-async function getPeople (id) {
+async function getPeople(id) {
     const people = await hitRememberizerApi(`/people/${id}`)
     model.batches[id].people = []
     people.forEach(person => {
@@ -46,7 +46,34 @@ async function getPeople (id) {
     })
 }
 
-async function init () {
+function calculateOverlap(batch) {
+    const myStart = new Date(model.me.batch.start_date)
+    const myEnd = new Date(model.me.batch.end_date)
+    const batchStart = new Date(batch.start_date)
+    const batchEnd = new Date(batch.end_date)
+    if (myStart < batchStart) {
+        if (myEnd < batchEnd) {
+            return myEnd - batchStart
+        } else {
+            return batchEnd - batchStart
+        }
+    }
+    if (myStart < batchEnd) {
+        if (myEnd < batchEnd) {
+            return myEnd - myStart
+        } else {
+            return batchEnd - myStart
+        }
+    }
+    return batchEnd - myStart
+}
+
+function getSortedBatches() {
+    const sortedBatches = Object.values(model.batches).sort((a, b) => calculateOverlap(b) - calculateOverlap(a))
+    return sortedBatches
+}
+
+async function init() {
     await Promise.all([getList(), getMe()])
     await getPeople(model.me.batch.id)
     console.log(JSON.parse(JSON.stringify(model)))
@@ -56,7 +83,17 @@ const clickMe = el => e => {
     init()
 }
 
+const loadPeople = id => el => async e => {
+    await getPeople(id)
+    console.log(JSON.parse(JSON.stringify(model)))
+}
+
 init()
 render(document.body, h`
     <span onclick=${clickMe}>init again</span>
+    ${mapEntries(getSortedBatches, batch => h`
+        <div onclick=${loadPeople(batch.id)}>
+            ${batch.name}
+        </div>
+    `)}
 `)
